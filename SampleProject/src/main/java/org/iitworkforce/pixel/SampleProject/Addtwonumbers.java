7e1fb5b3e2dc20c1b850738a4de92142342e8690
Ranji <Ranji@Aanie>
2025-06-02 12:50:10 -0400

    Committing three files

---
+++ SampleProject/src/main/java/org/iitworkforce/pixel/SampleProject/Addtwonumbers.java
@@ -0,0 +1,13 @@
+package org.iitworkforce.pixel.SampleProject;
+
+public class Addtwonumbers {
+
+	public static void main(String[] args) {
+		int a=30;
+		int b=20;
+				
+		int result = a+b;
+		System.out.println(result);
+	}
+
+}
